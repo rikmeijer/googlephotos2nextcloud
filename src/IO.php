4e18d874b6b2c1d1dc815c12dda1f9b6e48f85c3
Rik Meijer <hello@rikmeijer.nl>
2025-02-18 20:51:06 +0100

    include datetime in logging

--- src/IO.php
+++ src/IO.php
@@ -5,7 +5,7 @@ namespace Rikmeijer\Googlephotos2nextcloud;
 class IO {
 
     static function write(string $line): void {
-        print PHP_EOL . $line;
+        print PHP_EOL . '[' . date('Y-m-d H:i:s') . '] - ' . $line;
     }
 
     static function readJson(string $path): mixed {
