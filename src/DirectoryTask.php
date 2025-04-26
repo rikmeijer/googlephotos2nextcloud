804d94ee8a0e3a81d3299740552b15405bad5606
Rik Meijer <hello@rikmeijer.nl>
2025-04-26 17:38:25 +0200

    add delay to prevent eating resources

--- src/DirectoryTask.php
+++ src/DirectoryTask.php
@@ -113,7 +113,8 @@ readonly class DirectoryTask implements Task {
                 if ($attempts === 5) {
                     throw $e;
                 } else {
-                    $debug('attempt failed, retrying...');
+                    $debug('attempt failed, retrying in 10 seconds...');
+                    sleep(10);
                 }
             }
         } while ($attempts < 6);
